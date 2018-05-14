3f97193c24922803493a327d4eb8ad6101726333
cutiechi <chi@cutie.ooo>
2018-05-14 12:31:47 +0800

    feat(controller): add user controller

---
+++ src/main/java/com/cutiechi/demo/web/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.cutiechi.demo.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户控制器
+ *
+ * @author Cutie Chi
+ */
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+}
