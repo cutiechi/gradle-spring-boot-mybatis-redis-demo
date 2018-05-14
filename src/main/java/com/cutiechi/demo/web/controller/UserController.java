881ccbfe2b1e134e00d2fc59e5b904e175604dc7
cutiechi <chi@cutie.ooo>
2018-05-14 12:37:21 +0800

    feat(api): add insert user api

--- src/main/java/com/cutiechi/demo/web/controller/UserController.java
+++ src/main/java/com/cutiechi/demo/web/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.cutiechi.demo.web.controller;
 
+import com.cutiechi.demo.model.entity.User;
+import com.cutiechi.demo.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    /**
+     * 用户业务逻辑对象
+     */
+    private final UserService userService;
+
+    /**
+     * Spring 自动注入
+     *
+     * @param userService 用户业务逻辑对象
+     */
+    @Autowired
+    public UserController (UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * 添加用户
+     *
+     * @param user 用户
+     * @return 用户
+     */
+    @PostMapping("")
+    public User insert (@RequestBody final User user) {
+        return userService.insert(user);
+    }
 }
