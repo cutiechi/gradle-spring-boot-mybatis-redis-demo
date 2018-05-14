a89ad3920053b1426fb2184e66114c3c62bab171
cutiechi <chi@cutie.ooo>
2018-05-14 12:44:19 +0800

    feat(api): add get user by user id api

--- src/main/java/com/cutiechi/demo/web/controller/UserController.java
+++ src/main/java/com/cutiechi/demo/web/controller/UserController.java
@@ -5,6 +5,7 @@ import com.cutiechi.demo.service.UserService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -55,5 +56,16 @@ public class UserController {
     @GetMapping("")
     public List<User> listAll () {
         return userService.listAll();
+    }
+
+    /**
+     * 根据用户 ID 获取用户
+     *
+     * @param userId 用户 ID
+     * @return 用户
+     */
+    @GetMapping("/{userId}")
+    public User getById (@PathVariable final Integer userId) {
+        return userService.getById(userId);
     }
 }
