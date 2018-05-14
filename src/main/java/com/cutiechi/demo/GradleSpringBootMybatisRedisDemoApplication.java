5c27fdc27b6ac0c4d108bf6620a7e1dceb02c771
cutiechi <chi@cutie.ooo>
2018-05-14 09:30:06 +0800

    initial commit

---
+++ src/main/java/com/cutiechi/demo/GradleSpringBootMybatisRedisDemoApplication.java
@@ -0,0 +1,12 @@
+package com.cutiechi.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleSpringBootMybatisRedisDemoApplication {
+
+    public static void main (String[] args) {
+        SpringApplication.run(GradleSpringBootMybatisRedisDemoApplication.class, args);
+    }
+}
