64d89509b6b09490153c849e630ea4a2df1dbef4
cutiechi <chi@cutie.ooo>
2018-05-14 09:39:52 +0800

    refactor: use junit 5 to test application

--- src/test/java/com/cutiechi/demo/ApplicationTests.java
+++ src/test/java/com/cutiechi/demo/ApplicationTests.java
@@ -1,14 +1,14 @@
 package com.cutiechi.demo;
 
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 /**
  * 程序测试类
  */
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ApplicationTests {
 
