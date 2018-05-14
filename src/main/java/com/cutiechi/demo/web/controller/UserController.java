90d02e32a10cc11ec644a799837e2c1f4d056bcf
cutiechi <chi@cutie.ooo>
2018-05-14 12:45:01 +0800

    feat(api): add update user api

--- src/main/java/com/cutiechi/demo/web/controller/UserController.java
+++ src/main/java/com/cutiechi/demo/web/controller/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -67,5 +68,10 @@ public class UserController {
     @GetMapping("/{userId}")
     public User getById (@PathVariable final Integer userId) {
         return userService.getById(userId);
+    }
+
+    @PutMapping("")
+    public User update (@RequestBody User user) {
+        return userService.update(user);
     }
 }
