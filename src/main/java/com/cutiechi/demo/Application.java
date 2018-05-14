2263ed90acd2b2b5bcc3fed018e0bd6ec0a42c5a
cutiechi <chi@cutie.ooo>
2018-05-14 09:35:51 +0800

    refactor: rename application main class and main test class

--- src/main/java/com/cutiechi/demo/Application.java
+++ src/main/java/com/cutiechi/demo/Application.java
@@ -3,10 +3,15 @@ package com.cutiechi.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 程序启动类
+ *
+ * @author Cutie Chi
+ */
 @SpringBootApplication
-public class GradleSpringBootMybatisRedisDemoApplication {
+public class Application {
 
     public static void main (String[] args) {
-        SpringApplication.run(GradleSpringBootMybatisRedisDemoApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
